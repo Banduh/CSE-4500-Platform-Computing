29649f6673f92362b2ff97e41b4297f30e210dc9
Banduh <tranb0303@gmail.com>
2024-03-25 21:56:26 -0700

    write to database

--- Assignment_3_Database/Metric_tracker/metric_tracker.py
+++ Assignment_3_Database/Metric_tracker/metric_tracker.py
@@ -1,5 +1,7 @@
 import time
 from selenium import webdriver
+import collections
+import pymysql
 
 # Initialize browser
 driver = webdriver.Chrome()
@@ -7,32 +9,50 @@ driver = webdriver.Chrome()
 # Navigate to your website 
 driver.get("http://localhost:3000/")
 
+# Initialize variables
 metrics = []
-# Track presence time 
+SAMPLE_SIZE = 2
+count = 0
 start_time = time.time()
-presence_time = start_time
-while True:#presence_time < 50: # seconds
+while count < SAMPLE_SIZE:
+    # Track presence time 
     current_time = time.time()
     presence_time = current_time - start_time
     print(f"Presence time: {presence_time} seconds")
+    # metrics ["Presence time {Seconds}"].append(presence_time)
     
     # Track scrolling
     scroll_height = driver.execute_script("return document.body.scrollHeight")  
     current_scroll = driver.execute_script("return window.pageYOffset")
     print(f"Scrolled {current_scroll}/{scroll_height} pixels")
-    
+    # metrics ["Presence time {Seconds}"].append(presence_time)
+    metrics.append({"TIMESTAMP (HH:MM:SS)": time.strftime("%H:%M:%S", time.localtime()),
+                    "Presence time (seconds)" : presence_time, 
+                    "Scrolling (Pixels)" : current_scroll/scroll_height } )
+
+    count += 1
     time.sleep(2) 
+      
+driver.quit()
+print(metrics)
 
-    # Track clicks   
-    # buttons = driver.find_elements_by_tag_name("button")
-    # num_clicks = 0
+conn = pymysql.connect(
+    host = "127.0.0.1",
+    user = "root",
+    password = "BTPandas1591!",
+    db = "metrics"
+    charset = 'utf8mb4'
+    cursorclass=pymysql.cursors.Dictcursor
+)
 
-    # for button in buttons:
-    #     button.click()
-    #     num_clicks += 1
-        
-    # print(f"Number of clicks: {num_clicks}")
-        
-# driver.quit()
-while True:
-    print()
+try:
+    with conn.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `metrics` (`time`, `Scrolling`) VALUES ('presence_time', 'current_scroll/scroll_height')"
+
+    # Commit changes
+    conn.commit()
+
+    print("Record inserted successfully")
+finally:
+    conn.close()
